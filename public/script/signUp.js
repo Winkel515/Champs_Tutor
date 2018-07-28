7bd6858e0fc99c3cd1266e1fdc5c1466bddfb692
Winkel Yin <winkel515@hotmail.com>
2018-07-28 17:00:49 -0400

    Tutors can now setup short description in signup

--- public/script/signUp.js
+++ public/script/signUp.js
@@ -1,15 +1,10 @@
-function postData(config) {
-
-   return fetch('/tutors/signup', config)
+function checkStatus(response){
+  if(response.ok){
+      return Promise.resolve(response); 
+  } else{
+    return Promise.reject(response.text());
+  }
 }
-
-   function checkStatus(response){
-    if(response.ok){
-       return Promise.resolve(response); 
-    } else{
-     return Promise.reject(response.text());
-    }
-   }
 
 //    -------------------
 //    TUTOR VUE.JS FOR FORM VALIDATION
@@ -24,34 +19,45 @@ const app = new Vue({
       emailError: false,
       emailDuplicate: false,
       password: "",
-      passwordError:false
+      passwordError:false,
+      shortDescription: '',
+      shortDescriptionError: false
     },
     methods: {
       submitForm: function (e) {
          
         this.nameError = this.name.length === 0;
         this.emailError = !this.validEmail(this.email);
-        this.passwordError = this.password.length < 8;
         this.emailDuplicate = false;
+        this.passwordError = this.password.length < 8;
+        this.shortDescriptionError = this.shortDescription.length > 200;
         
         e.preventDefault();
-        if(!(this.nameError || this.passwordError || this.emailError)){
-            const config = {
-                method: 'POST' ,
-                headers: {'Content-type': 'application/json'},
-                body: JSON.stringify({name : this.name, email : this.email, password : this.password}) 
-               };
-            postData(config).then(checkStatus)
-               .then(response => { // Runs when all inputs are good
-                     console.log(response.headers.get('x-auth')); // Logging the JWT for now. Can be stored in sessionStorage or localStorage
-               }).catch(response => { // Runs when there's an invalid input
-                  response.then(e => {
-                        console.log(JSON.parse(e));
-                        if(JSON.parse(e).message === "Email is already in use"){ // Run if email is already in database
-                              this.emailDuplicate = true; // Boolean used to show error message on signUp page
-                        }
-                  })
-               })
+        // Checks for error before actually making the POST request
+        if(!(this.nameError || this.passwordError || this.emailError || this.shortDescriptionError)){
+          const config = {
+              method: 'POST' ,
+              headers: {'Content-type': 'application/json'},
+              body: JSON.stringify({
+                name : this.name,
+                email : this.email,
+                password : this.password,
+                shortDescription: this.shortDescription
+              }) 
+          };
+          fetch('/tutors/signup', config).then(checkStatus)
+          .then(response => { // Runs when all inputs are good
+            console.log(response.headers.get('x-auth')); // Logging the JWT for now. Can be stored in sessionStorage or localStorage
+            location.href = '/';
+          }).catch(response => { // Runs when there's an invalid input
+            response.then(e => {
+              console.log(JSON.parse(e));
+              if(JSON.parse(e).message === "Email is already in use"){ // Run if email is already in database
+                    this.emailDuplicate = true; // Boolean used to show error message on signUp page
+              }
+            })
+          });
+          
         }
       },
       validEmail: function (email) {
