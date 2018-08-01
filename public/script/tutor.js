d4bbd0bdeeeeeec560d2c8d8269cf2c3325b53d3
danielbucci <danielbucci2@gmail.com>
2018-08-01 09:19:31 -0400

    adds sign in functionality to tutor profile page

--- public/script/tutor.js
+++ public/script/tutor.js
@@ -14,13 +14,54 @@ function getTutor(){ // Gets tutor from /tutors and sets it up for Vue
   };
 };
 
+// ------------------------------------------
+//  HELPER FUNCTIONS
+// ------------------------------------------
+
+function checkStatus(response){
+  if(response.ok){
+     return Promise.resolve(response); 
+  } else{
+   return Promise.reject(response.text()); 
+  }
+ }
+
+// ------------------------------------------
+//  Vue.js template
+// ------------------------------------------
+
+
 function tutorsVue(tutor){
   const app = new Vue({
       el: '#tutorProfile',
       data: {
         tutor: tutor,
+        email: "",
+        password: ""
       },
-      
+      methods: {
+        signIn: function (e) {
+          e.preventDefault();
+          
+            const config = {
+                method: 'POST' ,
+                headers: {'Content-type': 'application/json'},
+                body: JSON.stringify({ 
+                  email : this.email,
+                  password : this.password, 
+                }) 
+            };
+            fetch('/tutors/login', config).then(checkStatus)
+            .then(response => { // Runs when all inputs are good
+              localStorage.setItem('token', response.headers.get('x-auth'));
+              console.log(response.headers.get('x-auth')); // Logging the JWT for now. Can be stored in sessionStorage or localStorage
+            }).catch(response => { // Runs when there's an invalid input
+              response.then(e => {
+                console.log(JSON.parse(e));
+              })
+            });
+        }
+      }
       
   });
 }
