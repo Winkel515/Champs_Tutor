9e8add25d860d00788b089ba963883fdb98c54bc
Winkel515 <winkel515@hotmail.com>
2018-08-20 11:16:03 -0400

    Navbar works everywhere

--- public/util/script/app.js
+++ public/util/script/app.js
@@ -1,22 +1,3 @@
-// axios.get("/tutors")
-//   .then(response => tutorsVue(response.data.tutors))
-//   .catch(e => console.log(e))
-
-// document.addEventListener("DOMContentLoaded", getTutors());
-
-// function getTutors(){ // Gets tutors from /tutors and sets it up for Vue
-//   // *************************************** GET REQUEST TO /tutors ROUTE ****************************
-//   var xmlhttp = new XMLHttpRequest();
-//   xmlhttp.open("GET", "/tutors", true);
-//   xmlhttp.send();
-//   xmlhttp.onreadystatechange = function() {
-//       if (this.readyState == 4 && this.status == 200) {
-//           var tutors = JSON.parse(this.responseText).tutors; // Setting up the tutors array
-//           tutorsVue(tutors) // Injects the tutors array into the Vue object
-//       };
-//   };
-// };
-
 // // ------------------------------------------
 // //  FETCH FUNCTIONS
 // // ------------------------------------------
@@ -57,31 +38,6 @@ function tutorsVue(tutors){
         screenSize: window.screen.width
       },
       methods: {
-        signIn: function (e) {
-          e.preventDefault();
-          this.invalidCredentials = false;
-          
-            const config = {
-                method: 'POST' ,
-                headers: {'Content-type': 'application/json'},
-                body: JSON.stringify({ 
-                  email : this.email,
-                  password : this.password, 
-                }) 
-            };
-            fetch('/tutors/login', config).then(checkStatus)
-            .then(response => { // Runs when all inputs are good
-              localStorage.setItem('token', response.headers.get('x-auth'));
-              console.log(response.headers.get('x-auth')); // Logging the JWT for now. Can be stored in sessionStorage or localStorage
-              this.isSignedIn = true;
-              $('#signIn').modal('hide');
-            }).catch(response => { // Runs when there's an invalid input
-              response.then(e => {
-                console.log(JSON.parse(e));
-                this.invalidCredentials = true;
-              })
-            });
-        },
         filterSubject: function(){
            /* Filter Subjects */ 
           console.log(this.screenSize);
@@ -101,19 +57,7 @@ function tutorsVue(tutors){
           console.log("Subject:", this.subject);
           console.log("Price:", this.price);
         },
-        logOut: function(){
-          localStorage.clear();
-          this.isSignedIn = false;
-            $('#logOut').modal('hide');
-        }
       },
-      beforeMount() { // Called right before the mounting begins: the render function is about to be called for the first time.this checks local storage
-          if (localStorage.getItem('token')) {
-            this.isSignedIn = true;
-          }  
-          console.log(localStorage.getItem('token'));
-      
-      }, 
       computed: {
           subjectList:function() {
               return  ['Calculus I', 'Calculus II', 'Calculus III','Mechanics', 'Electricity & Magnetism','Waves and Optics']
--- public/util/script/navbar.js
+++ public/util/script/navbar.js
@@ -1,5 +1,47 @@
-function redirectEdit() {
-    var token = localStorage.getItem('token'); 
-    pathName = jwt_decode(token)._id;
-    location.href = `/editProfile`;
-}
+new Vue({
+    el: "#navbar",
+    data: {
+        isSignedIn: false,
+        email:"",
+        password:"",
+        invalidCredentials: false,
+    },
+    methods: {
+        signIn: function (e) {
+            e.preventDefault();
+            this.invalidCredentials = false;
+            
+              const config = {
+                  method: 'POST' ,
+                  headers: {'Content-type': 'application/json'},
+                  body: JSON.stringify({ 
+                    email : this.email,
+                    password : this.password, 
+                  }) 
+              };
+              fetch('/tutors/login', config).then(checkStatus)
+              .then(response => { // Runs when all inputs are good
+                localStorage.setItem('token', response.headers.get('x-auth'));
+                console.log(response.headers.get('x-auth')); // Logging the JWT for now. Can be stored in sessionStorage or localStorage
+                this.isSignedIn = true;
+                $('#signIn').modal('hide');
+              }).catch(response => { // Runs when there's an invalid input
+                response.then(e => {
+                  console.log(JSON.parse(e));
+                  this.invalidCredentials = true;
+                })
+              });
+          },
+          logOut: function(){
+            localStorage.clear();
+            this.isSignedIn = false;
+              $('#logOut').modal('hide');
+          }
+    },
+    beforeMount() { // Called right before the mounting begins: the render function is about to be called for the first time.this checks local storage
+        if (localStorage.getItem('token')) {
+          this.isSignedIn = true;
+        }  
+        console.log(localStorage.getItem('token'));
+    },
+})
